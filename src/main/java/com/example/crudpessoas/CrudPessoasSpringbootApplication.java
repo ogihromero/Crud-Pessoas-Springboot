680d163f79ab48c70a3386d8741e4ed6e8d724e1
Adriano Romero <adriano.romerofco@gmail.com>
2023-01-09 17:02:40 -0300

    build: commit inicial

---
+++ src/main/java/com/example/crudpessoas/CrudPessoasSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPessoasSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudPessoasSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crudpessoas/CrudPessoasSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPessoasSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
